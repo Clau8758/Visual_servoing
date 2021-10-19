843be1aa46a5b0d7fd906e3cf93f011b386ec20c
Clau8758 <56257587+Clau8758@users.noreply.github.com>
2021-10-19 12:34:22 +0200

    Added comments

--- ibvs_visual_servoing.py
+++ ibvs_visual_servoing.py
@@ -14,13 +14,14 @@ import numpy as np
 import os
 import numpy.matlib
 
-ideal_feature=np.matlib.zeros((4*2,1))
 
+#Load the camera calibration parameters from the file "mtx_dist.npz". The file is generated using the calibrate_camera.py script.
 path = '/Users/Claus/python_scripts/'
 load = np.load(os.path.join(os.path.dirname(path), "mtx_dist.npz"))
 mtx = load["mtx"]
 dist = load["dist"]
 
+#Set the type of aruco marker the algorithm should search for by changing aruco."DICT_5X5_100"
 arucoDict = cv2.aruco.Dictionary_get(aruco.DICT_5X5_100)
 arucoParams = cv2.aruco.DetectorParameters_create()
 # initialize the video stream from the camera 2 sec sleep is added to varm up sensor
@@ -29,70 +30,64 @@ vs = VideoStream(src=0).start()
 time.sleep(2.0)
 
 
-
-#Define the target positions in image coordinates
+#Define the target positions in image coordinates. The target positions are the desired locations of each of the aruco markers corner points. 
 #top-left, top-right, bottom-right, and bottom-left order
 target_positions = np.array([(int((1280/2)-100),int((720/2)-100)),(int((1280/2)+100),int((720/2)-100)),(int((1280/2)+100),int((720/2)+100)),(int((1280/2)-100),int((720/2)+100))])
 
 
-#Add Camera calibration and distortion parameters
-
-
 while True:
-	# grab the frame from the threaded video stream and resize it
-	# to have a maximum width of 1000 pixels
+    #Grabs the frame from the threaded video stream and resizes it to have a maximum width of 1000 pixels
     frame = vs.read()
     frame = imutils.resize(frame, width=1000)
     #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	# detect ArUco markers in the input frame and output corner pixel coordinates
+    
+    #Detects ArUco markers in the input frame and outputs corner pixel coordinates
     (corners, ids, rejected) = cv2.aruco.detectMarkers(frame,arucoDict, parameters=arucoParams)
+    #The found corners are saved in the corners list in the order top-left, top-right, bottom-right, and bottom-left
     corners = np.array(corners)
     
-    # Calculate the distance to marker center which is Z in the interaction matrix (the Z-coord in the tvec)
-    markerSizeInCM = 5
+    #Calculate the distance to marker center by outputting tvec. Which is used to approximate Z or depth in the interaction matrix (Depth is magnitude of tvec)
+    #Using a 10 cm marker the output corresponds to depth in cm. Using markersize 5 means the output needs to be halfed to be correct in cm 
+    markerSizeInCM = 10
     rvec , tvec, _ = aruco.estimatePoseSingleMarkers(corners, markerSizeInCM, mtx, dist)
+    #If no marker is found the magnitude of tvec should be calculated
     if tvec is not None:
         mag = np.linalg.norm(tvec)
     
-    #Draw the desired position of the 4 corner points
+    #The following lines draws the desired position of the 4 corner points on the frame in BLUE
     #cv2.circle(frame, (target_positions[0][0],target_positions[0][1]), 4, (255, 0, 0), -1)
     #cv2.circle(frame, (target_positions[1][0],target_positions[1][1]), 4, (255, 0, 0), -1)
     #cv2.circle(frame, (target_positions[2][0],target_positions[2][1]), 4, (255, 0, 0), -1)
     #cv2.circle(frame, (target_positions[3][0],target_positions[3][1]), 4, (255, 0, 0), -1)    
 
-    
-    
-	# verify *at least* one ArUco marker was detected
+    #Verify that an ArUco marker was detected or else 
     if len(corners) > 0:
-		# flatten the ArUco IDs list
+	# flatten the ArUco IDs list
         ids = ids.flatten()
-		# loop over the detected ArUCo corners
+	# loop over the detected ArUCo corners
         for (markerCorner, markerID) in zip(corners, ids):
-			# The marker corners are always returned
-			# in top-left, top-right, bottom-right, and bottom-left
-			# order)
+	    # The marker corners are always returned
+	    # in top-left, top-right, bottom-right, and bottom-left order
             corners = markerCorner.reshape((4, 2))
             (topLeft, topRight, bottomRight, bottomLeft) = corners
-			# convert each of the (x, y)-coordinate pairs to integers
+	    # convert each of the (x, y)-coordinate pairs to integers
             topRight = (int(topRight[0]), int(topRight[1]))
             bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
             bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
             topLeft = (int(topLeft[0]), int(topLeft[1]))
             
-			# draw the bounding box of the ArUCo detection
+	    # draw the bounding box of the ArUCo detection
             cv2.line(frame, topLeft, topRight, (0, 255, 0), 2)
             cv2.line(frame, topRight, bottomRight, (0, 255, 0), 2)
             cv2.line(frame, bottomRight, bottomLeft, (0, 255, 0), 2)
             cv2.line(frame, bottomLeft, topLeft, (0, 255, 0), 2)
-			# compute and draw the center (x, y)-coordinates of the
-			# ArUco marker
+	    
+            # compute and draw the center (x, y)-coordinates of the
+	    # ArUco marker
             cX = int((topLeft[0] + bottomRight[0]) / 2.0)
             cY = int((topLeft[1] + bottomRight[1]) / 2.0)
             cv2.circle(frame, (cX, cY), 4, (0, 0, 255), -1)  
-            
-            
-                   
-         
+    #If no marker detected show frame and print "No marker detected"     
     else:
         print("[INFO] No marker detected...")
 	# show the output frame
@@ -102,26 +97,29 @@ while True:
     if key == ord("q"):
         break
     
+    #If a marker is found and the depth Z estimated the interaction matrix is generated
     if tvec is not None:    
-        #Generate L or interaction matrix:
         L=np.matlib.zeros((2*4,6))  
         # Gain on controller, essentially sets arm speed, although too high of a value will cause the
         # function to diverge.
+	lam = 0.5
+	
+	#Target feature are the detected corners
         target_feature = corners.flatten()
         target_feature = target_feature[:,None]
         
         target_positions = target_positions.flatten()
         target_positions = target_positions[:,None]
         
+	#Same depth are used for all four points (Probably no ideal and might not work)
         Z = mag
-        ideal_feature=np.matlib.zeros((4*2,1))
-        lam = 0.5
+	
         for i in range(0,4):
             x=corners[i][0]
             y=corners[i][1]
-            ideal_feature[i*2,0]=x
-            ideal_feature[i*2+1,0]=y
+	    #Generate L/interaction matrix which is a 8x6 matrix for 4 pts:
             L[i*2:i*2+2,:]=np.matrix([[-1/Z,0,x/Z,x*y,-(1+x*x),y],[0,-1/Z,y/Z,1+y*y,-x*y,-x]])
+	#The image feature error calculated in pixels is determined from the found corners and the desired corner positions
         error = target_feature - target_positions
         vel=-lam*np.dot(np.linalg.pinv(L),error)
         
